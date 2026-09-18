88310c4c70e5cd7a539d1bfc8988be364b0fed71
Sukhman3125 <sukhman3125@gmail.com>
2026-09-19 01:38:21 +0530

    fix: update cookie settings for login and logout to enhance security

--- backend/modules/auth/auth.controller.js
+++ backend/modules/auth/auth.controller.js
@@ -49,8 +49,8 @@ const login = asyncHandler(async (req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
@@ -184,8 +184,8 @@ const logout = asyncHandler(async (req, res) => {
 
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
   });
 
   res.status(200).json({
